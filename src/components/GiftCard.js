214c4816f72cccb547d8138a16d7e5eb7bb3311c
Steve <sgriesel@gmail.com>
2019-04-25 16:17:01 +0200

    added some elements

--- src/components/GiftCard.js
+++ src/components/GiftCard.js
@@ -3,8 +3,23 @@ import '../css/GiftCard.css';
 
 class GiftCard extends Component {
     render() {
-        return <div className="form">Form</div>
-    } 
+        return (
+            <div className="form-wrapper">
+                <div className="form">
+                    <h4>Gift Cards</h4>
+                    <form>
+                        <div className="gift-checkbox">
+                            <input type="checkbox" name="giftcard-checkbox" />Do you have a gift card?
+                        </div>
+                        <div className="gift-number">
+                            <input /><input />
+                        </div>
+                        <button className="apply-btn">APPLY</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default GiftCard;
