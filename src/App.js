d9b3912e282d3d469e69907544135e5679a0c953
Steve <sgriesel@gmail.com>
2019-04-25 15:17:50 +0200

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import GiftCard from './components/GiftCard';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GiftCard />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/GiftCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../css/GiftCard.css';
+
+class GiftCard extends Component {
+    render() {
+        return <div className="form">Form</div>
+    } 
+}
+
+export default GiftCard;
