da902375ee8f17fde3d9a102f321067176cfc9db
Steve <sgriesel@gmail.com>
2019-04-26 11:28:53 +0200

    add coupon logic, display result

--- src/components/Coupon.js
+++ src/components/Coupon.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class Coupon extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showResult: false
+        }
+        this.showResult = this.showResult.bind(this); // es6 option also possible
+    }
+
+    showResult() {
+        this.setState({
+            showResult: !this.state.showResult
+        });
+    }
     render(){
         return <div className="show-coupon">
         <p class="coupon-desc">Please enter the 19-digit number and code from your gift card below.</p>
+        {this.state.showResult ?
         <div className="result">
             <div className="result-left">
                 <p>Gift card</p>
@@ -13,6 +27,7 @@ class Coupon extends Component {
                 <p>-$20.00</p>
             </div>
         </div>
+        : null }
         <div className="gift-number">
             <input placeholder="Gift Card Number" /><input placeholder="Control Code" />
         </div>
