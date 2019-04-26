d2e2270c25c4a227d271d473a8bad7046948760b
Steve <sgriesel@gmail.com>
2019-04-26 09:26:27 +0200

    markup & styling done

--- src/components/GiftCard.js
+++ src/components/GiftCard.js
@@ -11,11 +11,23 @@ class GiftCard extends Component {
                         <div className="gift-checkbox">
                             <label class="container">Do you have a gift card?
                                 <input type="checkbox" name="giftcard-checkbox" />
-                                <span className="checkmark"></span>                            
+                                <span className="checkmark"></span>
                             </label>
                         </div>
-                        <div className="gift-number">
-                            <input /><input />
+                        <div className="show-coupon">
+                            <p class="coupon-desc">Please enter the 19-digit number and code from your gift card below.</p>
+                            <div className="result">
+                                <div className="result-left">
+                                    <p>Gift card</p>
+                                    <p>**** **** **** **** 123</p>
+                                </div>
+                                <div className="result-right">
+                                    <p>-$20.00</p>
+                                </div>
+                            </div>
+                            <div className="gift-number">
+                                <input placeholder="Gift Card Number" /><input placeholder="Control Code" />
+                            </div>
                         </div>
                         <button className="apply-btn">APPLY</button>
                     </form>
